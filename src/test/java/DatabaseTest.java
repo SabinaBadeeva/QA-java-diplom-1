937ad668d342ed69e2dd80e852f49556dc80e3b1
Sabina Badeeva <sabina-piter@list.ru>
2023-02-15 22:40:13 +0300

    add DatabaseTest

---
+++ src/test/java/DatabaseTest.java
@@ -0,0 +1,29 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import praktikum.Bun;
+import praktikum.Database;
+import praktikum.Ingredient;
+
+import java.util.List;
+
+public class DatabaseTest {
+    Database database;
+
+    @Before
+    public void setUp() {
+        database = new Database();
+    }
+
+    @Test
+    public void availableBunsTest() {
+        List<Bun> buns = database.availableBuns();
+        Assert.assertFalse(buns.isEmpty());
+    }
+
+    @Test
+    public void availableIngredientsTest() {
+        List<Ingredient> ingredients = database.availableIngredients();
+        Assert.assertFalse(ingredients.isEmpty());
+    }
+}
